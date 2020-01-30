919619cf0b1d1d4b3a9c5306d864c3010ae80255
ValkyrieCain <valkyrie.cain@qa.com>
2020-01-30 10:52:17 +0000

    second

---
+++ results.py
@@ -0,0 +1,9 @@
+physics=100
+chemistry=50
+maths=90
+total=physics+chemistry+maths
+per=total*100/450
+print("total marks")
+print(total)
+print("percentage")
+print(per)
---
+++ test3.py
@@ -0,0 +1,3 @@
+potato=12
+x=49
+print(potato+x)
