934bee96b1423e25e9bb507b8bf5e532de370299
ValkyrieCain <valkyrie.cain@qa.com>
2020-01-30 15:19:16 +0000

    30.01.20

---
+++ salary2.py
@@ -0,0 +1,11 @@
+name=input("Please enter your name ")
+salary=int(input("Please enter your salary "))
+if salary>2000:
+	tax=salary*21/100
+	net=salary-tax
+
+else:
+	tax=salary*15/100
+	net=salary-tax
+print("tax=",tax)
+print("net=",net)
