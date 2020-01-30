cdcb422be702467e3921f898e941c0fce5368589
ValkyrieCain <valkyrie.cain@qa.com>
2020-01-30 11:54:22 +0000

    second

---
+++ list1.py
@@ -0,0 +1,3 @@
+list1=[0,1,2,3,4,5,6,7,8,9]
+list1.insert(11,10)
+print(list1[int(input("What single positive numeric digit do you want to see? "))])
---
+++ maths.py
@@ -0,0 +1,8 @@
+print("Do some addition in your head!")
+one=int(input("First number to add: "))
+two=int(input("Second number to add: "))
+answer=int(input("What's the answer? "))
+if (one+two)==answer:
+	print("You got it right!")
+else:
+	print("That's not correct.")
