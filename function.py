52c396b7ba1e710effa073a2d0cd287dc17d725f
ValkyrieCain <valkyrie.cain@qa.com>
2020-01-30 16:46:06 +0000

    30.01.20

---
+++ function.py
@@ -0,0 +1,5 @@
+def fun1():
+	print ("x")
+fun1()
+if fun1() == print("x"):
+	print ("OK")
--- num.py
+++ num.py
@@ -1,8 +1,10 @@
-num=int(input("enter a number")
+num=int(input("enter a number"))
 if num>2000:
 	print("A")
 	if num>4000:
 		print("C")
+		if num>5000:
+			print("I")
 	else:
 		print("F")
 else:
