2ee67b7e983871a6a71e26d646422b4f7aeb593f
ValkyrieCain <valkyrie.cain@qa.com>
2020-01-30 10:56:01 +0000

    second

--- results.py
+++ results.py
@@ -3,7 +3,5 @@ chemistry=50
 maths=90
 total=physics+chemistry+maths
 per=total*100/450
-print("total marks")
-print(total)
-print("percentage")
-print(per)
+print("total marks",total)
+print("percentage",per)
