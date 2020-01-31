5ec30c29c2e2ac9ad19eddb928bc9a46a2c39317
ValkyrieCain <valkyrie.cain@qa.com>
2020-01-31 16:16:42 +0000

    31.01.20^

--- own code/ndigits.py
+++ own code/ndigits.py
@@ -17,5 +17,19 @@ while length>=1:
 	length-=1
 	var+=1
 dtotal=d[1]+d[2]+d[3]+d[4]+d[5]+d[6]+d[7]+d[8]+d[9]
+length=len(no)
+var=1
+dnum=[]
+while length>=1:
+	dnum.append(d[var])
+	length-=1
+	var+=1
+length=len(no)
+var=0
+while length>1:
+	print(dnum[var],"+")
+	length-=1
+	var+=1
+print(dnum[var])
 
 print("=",dtotal)
