dec12e584b76c0787537e63dedcefabff8825712
ValkyrieCain <valkyrie.cain@qa.com>
2020-01-31 12:06:22 +0000

    31.01.20

--- change.py
+++ change.py
@@ -4,43 +4,37 @@ change=paid-bill
 if change>=50:
 	fifty=int(change/50)
 	change=change%50
+	print("£50:",fifty)
 else:
 	fifty=0
 if change>=20:
 	twenty=int(change/20)
 	change=change%20
+	print("£20:",twenty)
 else:
 	twenty=0
 if change>=10:
 	ten=int(change/10)
 	change=change%10
+	print("£10:",ten)
 else:
 	ten=0
 if change>=5:
 	five=int(change/5)
 	change=change%5
+	print("£5:",five)
 else:
 	five=0
 if change>=2:
 	two=int(change/2)
 	change=change%2
+	print("£2:",two)
 else:
 	two=0
 if change>=1:
 	one=int(change/1)
+	print("£1:",one)
 else:
 	one=0
-if fifty!=0:
-	print("£50:",fifty)
-if twenty!=0:
-	print("£20:",twenty)
-if ten!=0:
-	print("£10:",ten)
-if five!=0:
-	print("£5:",five)
-if two!=0:
-	print("£2:",two)
-if one!=0:
-	print("£1:",one)
-totalchange=fifty*50+twenty*20+ten*10+five*5+two*2+one*1
+totalchange=fifty*50+twenty*20+ten*10+five*5+two*2+one
 print("Total change:",totalchange)
--- ndigits.py
+++ ndigits.py
@@ -1,10 +1,14 @@
 no=int(input("n digits "))
-a=int(no/10000)
+import math
+digits=int(math.log10(no))+1
+print(digits)
+a="1"
+while digits!=1:
+	a=(a+"0")
+	digits=digits-1
+print(a)
 b=(int(no/1000))%10
 c=(int(no/100))%10
 d=(int(no/10))%10
 e=no%10
-print(a,"+",b,"+",c,"+",d,"+",e,"=",a+b+c+d+e)
-import math
-digits = int(math.log10(no))+1
-print(digits)
+print(a,"+",b,"+",c,"+",d,"+",e,"=",b+c+d+e)
