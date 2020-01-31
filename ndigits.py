5040ebe1954e3a3228db8482a781bd1a6868cab3
ValkyrieCain <valkyrie.cain@qa.com>
2020-01-31 12:13:01 +0000

    31.01.20

--- ndigits.py
+++ ndigits.py
@@ -1,14 +1,22 @@
 no=int(input("n digits "))
-import math
-digits=int(math.log10(no))+1
-print(digits)
-a="1"
+def digits():
+	import math
+	digits=int(math.log10(no))+1
+digits()
+d1z="1"
 while digits!=1:
-	a=(a+"0")
+	d1z=(d1z+"0")
 	digits=digits-1
-print(a)
+d1=int(no/int(d1z))
+digits()
+d2z=1
+while digits!=2:
+	d2z=(d2z+"0")
+	digits=digits-1
+d2=(int(no/int(d2z)))/10
+digits()
 b=(int(no/1000))%10
 c=(int(no/100))%10
 d=(int(no/10))%10
-e=no%10
-print(a,"+",b,"+",c,"+",d,"+",e,"=",b+c+d+e)
+d5=no%10
+print(d1,"+",d2,"+",d3,"+",d4,"+",d5,"=",d1+d2+d3+d4+d5)
