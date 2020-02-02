c4af7f81c8c5106aeaecb02a02af2ac43ff3cdcc
ValkyrieCain <valkyrie.cain@qa.com>
2020-02-02 17:18:55 +0000

    02.02.20

--- own code/ndigits.py
+++ own code/ndigits.py
@@ -13,7 +13,6 @@ length=len(no)
 var=1
 while length>=1:
 	d[var]=int(no[var-1])
-	print(d[var],"+")
 	length-=1
 	var+=1
 dtotal=d[1]+d[2]+d[3]+d[4]+d[5]+d[6]+d[7]+d[8]+d[9]
@@ -31,5 +30,4 @@ while length>1:
 	length-=1
 	var+=1
 print(dnum[var])
-
 print("=",dtotal)
--- own code/perfectly normal question.py
+++ own code/perfectly normal question.py
@@ -1,5 +1,3 @@
 name=input("What is your name? (Please be truthful) ")
 if "ic" in name:
 	print("You've been prank'd!!")
-else:
-	pass
