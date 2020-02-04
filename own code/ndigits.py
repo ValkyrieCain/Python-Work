817860350c684ed88e751763f1b063b26384e074
ValkyrieCain <valkyrie.cain@qa.com>
2020-02-04 10:55:12 +0000

    04.02.20

--- own code/ndigits.py
+++ own code/ndigits.py
@@ -2,11 +2,13 @@ no=input("n digits ")
 d={1:0,2:0,3:0,4:0,5:0,6:0,7:0,8:0,9:0}
 length=len(no)
 var=1
+dfinal=""
 while length>1:
 	d[var]=int(no[var-1])
-	print(d[var],"+")
+	dfinal=dfinal+str(d[var])+" + "
 	length-=1
 	var+=1
 d[var]=int(no[var-1])
+dfinal=dfinal+str(d[var])
 dtotal=d[1]+d[2]+d[3]+d[4]+d[5]+d[6]+d[7]+d[8]+d[9]
-print(d[var],"=",dtotal)
+print(dfinal,"=",dtotal)
